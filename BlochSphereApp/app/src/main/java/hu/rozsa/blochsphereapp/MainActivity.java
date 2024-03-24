6ed38d73df18fd4b5f6b9877f074122678f7369c
bakosrozsa <bakosrozsa01@gmail.com>
2024-03-24 02:02:04 +0100

    Del unused imports

--- BlochSphereApp/app/src/main/java/hu/rozsa/blochsphereapp/MainActivity.java
+++ BlochSphereApp/app/src/main/java/hu/rozsa/blochsphereapp/MainActivity.java
@@ -9,21 +9,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
